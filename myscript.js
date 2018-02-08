22b0e9175b5e0ce1670500526497d59de90c4d89
Sanchit Mendiratta <sanchit.zidane@gmail.com>
2018-02-09 02:19:08 +0530

    Some Refactoring and Button to Add Columns

--- myscript.js
+++ myscript.js
@@ -15,26 +15,73 @@ function excelSheet(m, n) {
         this.oData[iIndex] = new Array(n);
     }
 }
-
+/**
+ * 
+ * @param  {Object} oDomRef
+ */
 excelSheet.prototype.renderAt = function (oDomRef) {
     this.oDom = oDomRef;
     this.generateSheet(this.iRows, this.iColumns, oDomRef);
 }
 
+/**
+ * 
+ * @param {Integer} m 
+ * @param {Integer} n 
+ * @param {Object} oDomRef 
+ */
+excelSheet.prototype.generateSheet = function (m, n, oDomRef) {
+    var oBody = oDomRef;
+
+    // Get toolbar
+    var oToolbar = this.getToolbar();
+
+    this.oTable = document.createElement("table");
+    // Create headers
+    var oHeaderRow = document.createElement("tr");
+    oHeaderRow.className = "tableHeader";
+    for (var iHeader = 1; iHeader <= m; iHeader++) {
+        var oHeader = document.createElement("th");
+        oHeader.innerHTML = (iHeader);
+        oHeaderRow.appendChild(oHeader);
+    }
+    this.oTable.appendChild(oHeaderRow);
+    for (var iRow = 1; iRow <= m; iRow++) {
+        var oRow = this.getRow(iRow);
+        this.oTable.appendChild(oRow);
+    }
+    oBody.appendChild(oToolbar);
+    oBody.appendChild(this.oTable);
+}
+
 excelSheet.prototype.getToolbar = function () {
     var oToolbar = document.createElement("div");
-    var oAddBtn = document.createElement("button");
-    oAddBtn.onclick = this.addRow.bind(this);
-    oAddBtn.innerHTML = 'Add';
-    oToolbar.appendChild(oAddBtn);
+    var oAddRowBtn = document.createElement("button");
+    oAddRowBtn.onclick = this.addRow.bind(this);
+    oAddRowBtn.innerHTML = 'Add Row';
+    oToolbar.appendChild(oAddRowBtn);
+    var oAddColBtn = document.createElement("button");
+    oAddColBtn.onclick = this.addColumn.bind(this);
+    oAddColBtn.innerHTML = 'Add Column';
+    oToolbar.appendChild(oAddColBtn);
     return oToolbar;
+};
+
+excelSheet.prototype.addRow = function () {
+    var iRowNum = this.oData.push(new Array(this.iColumns));
+    var oRow = this.getRow(iRowNum);
+    this.oTable.appendChild(oRow);
+};
+
+excelSheet.prototype.addColumn = function () {
+   // write code to add a new column
 };
 
 excelSheet.prototype.getRow = function (iRow) {
     var oRow = document.createElement("tr");
-    oRow.setAttribute("data-row", (iRow + 1));
+    oRow.setAttribute("data-row", (iRow));
     oRow.className = "row";
-    for (var iColumn = 0; iColumn < n; iColumn++) {
+    for (var iColumn = 1; iColumn <= n; iColumn++) {
         var oColumn = document.createElement("td");
         oColumn.setAttribute("data-col", iColumn);
         oColumn.setAttribute("data-row", iRow);
@@ -48,38 +95,8 @@ excelSheet.prototype.getRow = function (iRow) {
     return oRow;
 };
 
-excelSheet.prototype.generateSheet = function (m, n, oDomRef) {
-    var oBody = oDomRef;
-
-    // Get toolbar
-    var oToolbar = this.getToolbar();
-
-    this.oTable = document.createElement("table");
-    // Create headers
-    var oHeaderRow = document.createElement("tr");
-    oHeaderRow.className = "tableHeader";
-    for (var iHeader = 0; iHeader < m; iHeader++) {
-        var oHeader = document.createElement("th");
-        oHeader.innerHTML = (iHeader + 1);
-        oHeaderRow.appendChild(oHeader);
-    }
-    this.oTable.appendChild(oHeaderRow);
-    for (var iRow = 0; iRow < m; iRow++) {
-        var oRow = this.getRow(iRow);
-        this.oTable.appendChild(oRow);
-    }
-    oBody.appendChild(oToolbar);
-    oBody.appendChild(this.oTable);
-}
-
 excelSheet.prototype.getValue = function (iRow, iColumn) {
-    return this.oData[iRow][iColumn] ? this.oData[iRow][iColumn] : "";
-};
-
-excelSheet.prototype.addRow = function () {
-    var iRowNum = this.oData.push(new Array(this.iColumns));
-    var oRow = this.getRow(iRowNum - 1);
-    this.oTable.appendChild(oRow);
+    return this.oData[iRow - 1][iColumn - 1] ? this.oData[iRow - 1][iColumn - 1] : "";
 };
 
 // Assign Initial Rows and Columns
